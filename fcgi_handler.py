9c4865b1777a30be89fae9c2e1947a0e649e4d3d
Albert Visser <visser@linuxmoby>
2010-10-03 12:34:53 +0200

    handlers voor wsgi en fastcgi toegevoegd

---
+++ fcgi_handler.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'doctool.settings'
+
+from django.core.servers.fastcgi import runfastcgi
+
+runfastcgi(method="threaded", daemonize="false")
